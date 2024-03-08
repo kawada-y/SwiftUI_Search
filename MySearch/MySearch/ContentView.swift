a85c4d79cee11ce2370d648038efc3860433ba8c
yoshiyuki.kawada <kawada-y@illmatics.co.jp>
2024-03-08 17:54:24 +0900

    検索アプリ作成中 3月8日

--- MySearch/MySearch/ContentView.swift
+++ MySearch/MySearch/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    // OkashiDataを参照する状態変数
+    @StateObject var okashiDataList = OkashiData()
+    // 入力された文字列を保持する状態変数
+    @State var inputText = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // 文字を受け取るTextFieldを表示する
+            TextField("キーワード",
+                      text: $inputText,
+                      prompt: Text("キーワードを入力してください"))
+                .onSubmit {
+                    // Taskは非同期で処理を実行できる
+                    Task {
+                        // 入力完了直後に検索する
+                        await okashiDataList.searchOkashi(keyword: inputText)
+                    }
+            }
         }
         .padding()
     }
---
+++ MySearch/MySearch/OkashiData.swift
@@ -0,0 +1,17 @@
+//
+//  OkashiData.swift
+//  MySearch
+//
+//  Created by 河田佳之 on 2024/03/08.
+//
+
+import Foundation
+
+// お菓子データ検索用クラス
+class OkashiData: ObservableObject {
+    // Web API検索用メソッド
+    func searchOkashi(keyword: String) async {
+        // デバッグ出力
+        print(keyword)
+    }
+}
