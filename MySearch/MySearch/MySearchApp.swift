0857a0d3edc4da808fe619d8e6739710678b30a0
yoshiyuki.kawada <kawada-y@illmatics.co.jp>
2024-03-08 17:22:50 +0900

    プロジェクト作成

---
+++ MySearch/MySearch/MySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySearchApp.swift
+//  MySearch
+//
+//  Created by 河田佳之 on 2024/03/08.
+//
+
+import SwiftUI
+
+@main
+struct MySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
